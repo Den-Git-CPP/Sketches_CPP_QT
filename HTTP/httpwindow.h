4671b6d6ac65bb3410b0f51904f02f60e138f547
Den-Git-CPP <madim-neodart@rambler.ru>
2022-12-26 20:03:52 +0300

    HTTP Example

--- HTTP/httpwindow.h
+++ HTTP/httpwindow.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2020 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the examples of the Qt Toolkit.
@@ -48,25 +48,73 @@
 **
 ****************************************************************************/
 
-#include <QApplication>
-#include <QDir>
-#include <QScreen>
+#ifndef HTTPWINDOW_H
+#define HTTPWINDOW_H
 
-#include "httpwindow.h"
+#include <QProgressDialog>
+#include <QNetworkAccessManager>
+#include <QUrl>
 
-int main(int argc, char *argv[])
+#include <memory>
+
+QT_BEGIN_NAMESPACE
+class QFile;
+class QLabel;
+class QLineEdit;
+class QPushButton;
+class QSslError;
+class QAuthenticator;
+class QNetworkReply;
+class QCheckBox;
+
+QT_END_NAMESPACE
+
+class ProgressDialog : public QProgressDialog {
+    Q_OBJECT
+
+public:
+    explicit ProgressDialog(const QUrl &url, QWidget *parent = nullptr);
+
+public slots:
+   void networkReplyProgress(qint64 bytesRead, qint64 totalBytes);
+};
+
+class HttpWindow : public QDialog
 {
-    QApplication app(argc, argv);
-    
-    HttpWindow httpWin;
-    const QRect availableSize = httpWin.screen()->availableGeometry();
-    httpWin.resize(availableSize.width() / 5, availableSize.height() / 5);
-    httpWin.move((availableSize.width() - httpWin.width()) / 2, (availableSize.height() - httpWin.height()) / 2);
-#ifdef Q_OS_ANDROID
-    httpWin.showMaximized();
-#else
-    httpWin.show();
+    Q_OBJECT
+
+public:
+    explicit HttpWindow(QWidget *parent = nullptr);
+    ~HttpWindow();
+
+    void startRequest(const QUrl &requestedUrl);
+
+private slots:
+    void downloadFile();
+    void cancelDownload();
+    void httpFinished();
+    void httpReadyRead();
+    void enableDownloadButton();
+    void slotAuthenticationRequired(QNetworkReply *, QAuthenticator *authenticator);
+#if QT_CONFIG(ssl)
+    void sslErrors(const QList<QSslError> &errors);
 #endif
 
-    return app.exec();
-}
+private:
+    std::unique_ptr<QFile> openFileForWrite(const QString &fileName);
+
+    QLabel *statusLabel;
+    QLineEdit *urlLineEdit;
+    QPushButton *downloadButton;
+    QCheckBox *launchCheckBox;
+    QLineEdit *defaultFileLineEdit;
+    QLineEdit *downloadDirectoryLineEdit;
+
+    QUrl url;
+    QNetworkAccessManager qnam;
+    QScopedPointer<QNetworkReply, QScopedPointerDeleteLater> reply;
+    std::unique_ptr<QFile> file;
+    bool httpRequestAborted = false;
+};
+
+#endif
