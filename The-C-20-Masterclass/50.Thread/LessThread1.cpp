a5ea9cd68a17bb2fdaec4036859072b1ae3a6511
DArt <madim-neodart@rambler.ru>
2023-10-18 23:24:04 +0300

    Thread sleep 3s

--- The-C-20-Masterclass/50.Thread/LessThread1.cpp
+++ The-C-20-Masterclass/50.Thread/LessThread1.cpp
@@ -8,9 +8,11 @@ using namespace std;
 int main(int argc, const char** argv)
 {
  setlocale(LC_ALL,"ru");
- cout<<"Start main\n";
- cout<<this_thread::get_id()<<"\tmain\n";
- cout<<"End main\n";
-   
+ for (size_t i = 0; i < 10; i++)
+ {
+  cout<<"ID thread - "<<this_thread::get_id()<<"\tmain\n";
+  this_thread::sleep_for(chrono::milliseconds(3000));
+  }
+  
 return 0;
 };
