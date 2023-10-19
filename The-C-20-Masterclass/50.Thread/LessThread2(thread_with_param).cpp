e0dbc4de0f482b7ad21e9b0f01f08181a2c0e5f2
DArt <madim-neodart@rambler.ru>
2023-10-19 20:56:08 +0300

    Add func HardWork with param a,b

--- The-C-20-Masterclass/50.Thread/LessThread2(thread_with_param).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread2(thread_with_param).cpp
@@ -4,7 +4,12 @@
 using namespace std;
 
 void HardWork(int a,int b){
- 
+ this_thread::sleep_for(chrono::milliseconds(1000)); // do 1s
+ cout<<"==========\tHardWork START\t==========";
+ this_thread::sleep_for(chrono::milliseconds(2000)); // do 2s
+ cout<<"HardWork Sum \t="<<a+b;
+ this_thread::sleep_for(chrono::milliseconds(2000)); // do 1s
+ cout<<"==========\tHardWork END\t==========";
 }
 
 int main(int argc, const char** argv)
