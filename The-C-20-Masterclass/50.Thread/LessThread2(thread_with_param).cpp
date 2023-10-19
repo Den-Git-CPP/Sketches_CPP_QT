997ddf738c13e66613cb25a162e3bf7ce2d14207
DArt <madim-neodart@rambler.ru>
2023-10-19 21:00:24 +0300

    Add th1 with param

--- The-C-20-Masterclass/50.Thread/LessThread2(thread_with_param).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread2(thread_with_param).cpp
@@ -9,17 +9,21 @@ void HardWork(int a,int b){
  this_thread::sleep_for(chrono::milliseconds(2000)); // do 2s
  cout<<"HardWork Sum \t="<<a+b;
  this_thread::sleep_for(chrono::milliseconds(2000)); // do 1s
- cout<<"==========\tHardWork END\t==========";
+ cout<<"==========\tHardWork STOP\t==========";
 }
 
 int main(int argc, const char** argv)
 {
  setlocale(LC_ALL,"ru");
+
+
+ thread th1(HardWork,2,5);
+
  for (size_t i = 0; i < 10; i++)
  {
   cout<<"ID thread - "<<this_thread::get_id()<<"\tmain\n";
   this_thread::sleep_for(chrono::milliseconds(500));// do 0,5 sec
   }
- 
+ th1.join();
 return 0;
 };
