e00d6250074a58c194f5e59e1318fa64d32466e2
DArt <madim-neodart@rambler.ru>
2023-10-19 20:58:20 +0300

    Add main Thread

--- The-C-20-Masterclass/50.Thread/LessThread2(thread_with_param).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread2(thread_with_param).cpp
@@ -15,6 +15,11 @@ void HardWork(int a,int b){
 int main(int argc, const char** argv)
 {
  setlocale(LC_ALL,"ru");
+ for (size_t i = 0; i < 10; i++)
+ {
+  cout<<"ID thread - "<<this_thread::get_id()<<"\tmain\n";
+  this_thread::sleep_for(chrono::milliseconds(500));// do 0,5 sec
+  }
  
 return 0;
 };
