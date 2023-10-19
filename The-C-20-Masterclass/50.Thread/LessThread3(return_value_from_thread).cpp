07499ce8589248501125f8e98583d82677f6431c
DArt <madim-neodart@rambler.ru>
2023-10-19 21:31:36 +0300

    Add Return value over lambda

--- The-C-20-Masterclass/50.Thread/LessThread3(return_value_from_thread).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread3(return_value_from_thread).cpp
@@ -23,9 +23,11 @@ return a+b;
 int main(int argc, const char** argv)
 {
  setlocale(LC_ALL,"ru");
+/*==========================
+*/
 int Z{5};
 thread th_z(HardWork1,std::ref(Z));
-for (size_t i = 0; i < 10; i++)
+for (size_t i = 0; i < 5; i++)
  {
   cout<<"ID thread - "<<this_thread::get_id()<<"\tmain\n";
   this_thread::sleep_for(chrono::milliseconds(500));// do 0,5 sec
@@ -33,6 +35,23 @@ for (size_t i = 0; i < 10; i++)
 
 th_z.join();
 cout<<"==========\tZ="<<Z<<" \t==========";
-  
+
+/*==========================
+*/ 
+int result1=HardWork_Sum(3,3);
+cout<<"Function int HardWork_Sum"<< result1<<"\n";
+int result2{0};
+thread th_result(
+    [&result2](){result2= HardWork_Sum(4,4);}
+);
+
+for (size_t i = 0; i < 5; i++)
+ {
+  cout<<"ID thread - "<<this_thread::get_id()<<"\tmain\n";
+  this_thread::sleep_for(chrono::milliseconds(500));// do 0,5 sec
+  }
+th_result.join();
+cout<<"Function int HardWork_Sum over thread"<< result2<<"\n";
+
 return 0;
 };
