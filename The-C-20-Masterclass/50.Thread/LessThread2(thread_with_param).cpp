29d7f7b17b0c41c0a312d1ef3b5efe7a54c8c073
DArt <madim-neodart@rambler.ru>
2023-10-19 20:49:39 +0300

    Add Less2 Thread_with_Param

---
+++ The-C-20-Masterclass/50.Thread/LessThread2(thread_with_param).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+void HardWork(int a,int b){
+ 
+}
+
+int main(int argc, const char** argv)
+{
+ setlocale(LC_ALL,"ru");
+ 
+return 0;
+};
