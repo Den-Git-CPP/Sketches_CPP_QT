dfabf8cf70214ea20c596f8cce1d04874aacdef4
DArt <madim-neodart@rambler.ru>
2023-10-19 22:04:32 +0300

    Correct MuClass::Work3()

--- The-C-20-Masterclass/50.Thread/LessThread4(Class).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread4(Class).cpp
@@ -19,11 +19,20 @@ public:
     this_thread::sleep_for(chrono::milliseconds(2000)); // do 2s
     cout<<"==========\tMyClass::Work2 STOP\tID-\t"<<this_thread::get_id()<<" ==========\n"; 
     }
+    int Work3(int a,int b){
+    this_thread::sleep_for(chrono::milliseconds(2000)); // do 2s
+    cout<<"==========\tMyClass::Work2 START\tID-\t"<<this_thread::get_id()<<" ==========\n";
+    this_thread::sleep_for(chrono::milliseconds(2000)); // do 2s
+    cout<<"==========\tMyClass::Work2 STOP\tID-\t"<<this_thread::get_id()<<" ==========\n"; 
+    return a+b;
+    }
 };
 
 int main(int argc, const char** argv)
 {
  setlocale(LC_ALL,"ru");
+
+ 
 
 return 0;
 };
