ff506374d168cbb8027abc3418a373445784109d
DArt <madim-neodart@rambler.ru>
2023-10-19 21:21:30 +0300

    Add HardWork_Sum

--- The-C-20-Masterclass/50.Thread/LessThread3(return_value_from_thread).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread3(return_value_from_thread).cpp
@@ -8,8 +8,16 @@ this_thread::sleep_for(chrono::milliseconds(1000)); // do 1s
  cout<<"==========\tHardWork START\t==========";
  this_thread::sleep_for(chrono::milliseconds(2000)); // do 2s
  a=a*2;
- this_thread::sleep_for(chrono::milliseconds(2000)); // do 1s
+ this_thread::sleep_for(chrono::milliseconds(1000)); // do 1s
  cout<<"==========\tHardWork STOP\t==========";
+}
+
+int HardWork_Sum(int a,int b){
+this_thread::sleep_for(chrono::milliseconds(1000)); // do 1s
+ cout<<"==========\tHardWork START\tID-\t"<<this_thread::get_id()<<" ==========";
+ this_thread::sleep_for(chrono::milliseconds(2000)); // do 2s
+ cout<<"==========\tHardWork STOP\tID-\t"<<this_thread::get_id()<<" ==========";
+return a+b;
 }
 
 int main(int argc, const char** argv)
