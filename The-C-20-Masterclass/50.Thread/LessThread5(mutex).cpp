6a6e3d9f7932c07ee8e106d01a2d1b446548d557
DArt <madim-neodart@rambler.ru>
2023-10-20 23:50:50 +0300

    Add void Print

--- The-C-20-Masterclass/50.Thread/LessThread5(mutex).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread5(mutex).cpp
@@ -4,6 +4,19 @@
  
 using namespace std;
  
+void Print(char ch){
+    for (int i{0};i<5;++i){
+
+        for (int j {0} ; j < 10; j++)
+        {
+            cout<<ch;
+            this_thread::sleep_for(chrono::milliseconds(20));
+        }
+        cout<<"\n";
+    }
+    cout<<"\n";
+}
+
 int main(int argc, const char** argv)
 {
     setlocale(LC_ALL,"ru");
