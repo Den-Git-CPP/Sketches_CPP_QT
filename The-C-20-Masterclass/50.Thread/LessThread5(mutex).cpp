6e8e4394b62de5dd8e825d7b1c991bc093c2442c
DArt <madim-neodart@rambler.ru>
2023-10-21 00:06:37 +0300

    add Lock_Guard

--- The-C-20-Masterclass/50.Thread/LessThread5(mutex).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread5(mutex).cpp
@@ -22,8 +22,7 @@ void Print(char ch){
 }
 
 void Print_Lock_Guard(char ch){
-    
-   
+    lock_guard<mutex> guard(mtx2);/// start mutex2
     for (int i{0};i<5;++i){
         for (int j {0} ; j < 10; j++){
             cout<<ch;
@@ -32,8 +31,7 @@ void Print_Lock_Guard(char ch){
         cout<<"\n";
     }
     cout<<"\n";
-    
-   
+    /// stop mutex2
 }
 
 
@@ -42,11 +40,15 @@ int main(int argc, const char** argv)
  setlocale(LC_ALL,"ru");
  thread th1(Print,'*');
  thread th2(Print,'#');
+ thread th3(Print_Lock_Guard,'*');
+ thread th4(Print_Lock_Guard,'#');
  
  // Print('*');
  // Print('#');
   th1.join();
   th2.join();
+  th3.join();
+  th4.join();
 
 system("pause"); 
 return 0;
