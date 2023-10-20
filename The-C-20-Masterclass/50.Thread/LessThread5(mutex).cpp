60c4dbca7ec5ba9e71d52bc909ee467330cfd69b
DArt <madim-neodart@rambler.ru>
2023-10-20 23:59:04 +0300

    Print two threads

--- The-C-20-Masterclass/50.Thread/LessThread5(mutex).cpp
+++ The-C-20-Masterclass/50.Thread/LessThread5(mutex).cpp
@@ -3,8 +3,10 @@
 #include <mutex>
  
 using namespace std;
- 
+ mutex mtx1;
 void Print(char ch){
+    
+    mtx1.lock(); /// start mutex
     for (int i{0};i<5;++i){
         for (int j {0} ; j < 10; j++){
             cout<<ch;
@@ -13,14 +15,21 @@ void Print(char ch){
         cout<<"\n";
     }
     cout<<"\n";
+    
+    mtx1.unlock();/// stop mutex
 }
 
 int main(int argc, const char** argv)
 {
  setlocale(LC_ALL,"ru");
- Print('*');
- Print('#');
+ thread th1(Print,'*');
+ thread th2(Print,'#');
  
+ //Print('*');
+ // Print('#');
+  th1.join();
+  th2.join();
+  
 system("pause"); 
 return 0;
 };
