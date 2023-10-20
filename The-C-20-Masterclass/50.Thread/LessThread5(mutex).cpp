b9182a39d1adf7c0bc7210660e9225f99f427465
DArt <madim-neodart@rambler.ru>
2023-10-20 23:44:36 +0300

    Add Less 5 Mutex

---
+++ The-C-20-Masterclass/50.Thread/LessThread5(mutex).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+ 
+using namespace std;
+ 
+int main(int argc, const char** argv)
+{
+    setlocale(LC_ALL,"ru");
+ 
+ 
+ 
+ 
+ 
+system("pause"); 
+return 0;
+};
