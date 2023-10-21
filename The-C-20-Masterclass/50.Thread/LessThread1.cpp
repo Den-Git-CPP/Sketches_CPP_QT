0dd446682468bf0ddf54fe5bece9a90086a2f965
DArt <madim-neodart@rambler.ru>
2023-10-22 01:10:53 +0300

    Add Lambda Threads in Less1

--- The-C-20-Masterclass/50.Thread/LessThread1.cpp
+++ The-C-20-Masterclass/50.Thread/LessThread1.cpp
@@ -22,6 +22,14 @@ int main(int argc, const char** argv)
   cout<<"ID thread - "<<this_thread::get_id()<<"\tmain\n";
   this_thread::sleep_for(chrono::milliseconds(500));// do 0,5 sec
   }
+
+//Lambda
+  std::thread threadObj([] {
+		for (int i = 0; i < 10; i++)
+			std::cout << "Display Thread Executing" << std::endl;
+						  });
+ 
+ threadObj.join();
  
  t1.join();// join end thread t1 
  // t1.detach();
