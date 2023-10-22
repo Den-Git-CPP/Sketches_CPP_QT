471c355932bc571519d8380635d596d76ca64990
DArt <madim-neodart@rambler.ru>
2023-10-23 00:04:11 +0300

    Add LaineEdit Grid

--- Sketches_QT/Qt_FireBird/QT_PhoneBook/source/qt_phonebook.cpp
+++ Sketches_QT/Qt_FireBird/QT_PhoneBook/source/qt_phonebook.cpp
@@ -4,16 +4,22 @@ QT_PhoneBook::QT_PhoneBook (QWidget* parent) : QWidget (parent)
 {
     this->setWindowTitle ("Поиск по базе данных");
     this->resize (350, 150);
-    lbl_FIO        = new QLabel ("ФИО", this);
-    lbl_TlfNumber  = new QLabel ("Номер телефона", this);
-    lbl_Department = new QLabel ("Депртамент", this);
+    lbl_FIO           = new QLabel ("ФИО", this);
+    l_edit_FIO        = new QLineEdit (this);
+    lbl_TlfNumber     = new QLabel ("Номер телефона", this);
+    l_edit_TlfNumber  = new QLineEdit (this);
+    lbl_Department    = new QLabel ("Депртамент", this);
+    l_edit_Department = new QLineEdit (this);
 
-    vbox = new QVBoxLayout (this);
-    vbox->addWidget (lbl_FIO);
-    vbox->addWidget (lbl_TlfNumber);
-    vbox->addWidget (lbl_Department);
+    grid = new QGridLayout (this);
+    grid->addWidget (lbl_FIO, 0, 0);
+    grid->addWidget (l_edit_FIO, 0, 1);
+    grid->addWidget (lbl_TlfNumber, 1, 0);
+    grid->addWidget (l_edit_TlfNumber, 1, 1);
+    grid->addWidget (lbl_Department, 2, 0);
+    grid->addWidget (l_edit_Department, 2, 1);
 
-    this->setLayout (vbox);
+    this->setLayout (grid);
 }
 
 QT_PhoneBook::~QT_PhoneBook () {}
