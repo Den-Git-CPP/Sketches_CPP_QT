6de777aab78206040d0d567d577c9e851d1dd92c
DArt <madim-neodart@rambler.ru>
2023-10-25 00:43:18 +0300

    Add connect BD

--- Sketches_QT/Qt_DataBase/Qt_FireBird/QT_PhoneBook/include/qt_phonebook.h
+++ Sketches_QT/Qt_DataBase/Qt_FireBird/QT_PhoneBook/include/qt_phonebook.h
@@ -2,17 +2,26 @@
 
 #include <QMainWindow>
 
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlTableModel>
+#include <Qstring>
+#include <QDebug>
+
 namespace Ui {
-class QT_PhoneBook;
+    class QT_PhoneBook;
 }
 
 class QT_PhoneBook : public QMainWindow {
-  Q_OBJECT
+    Q_OBJECT
 
-public:
-  QT_PhoneBook(QWidget *parent = nullptr);
-  ~QT_PhoneBook();
+  public:
+    QT_PhoneBook (QWidget* parent = nullptr);
+    ~QT_PhoneBook ();
 
-private:
-  Ui::QT_PhoneBook *ui;
+  private:
+    Ui::QT_PhoneBook* ui;
+    QSqlDatabase db;
 };
--- Sketches_QT/Qt_DataBase/Qt_FireBird/QT_PhoneBook/source/qt_phonebook.cpp
+++ Sketches_QT/Qt_DataBase/Qt_FireBird/QT_PhoneBook/source/qt_phonebook.cpp
@@ -5,6 +5,23 @@ QT_PhoneBook::QT_PhoneBook (QWidget* parent) : QMainWindow (parent), ui (new Ui:
 {
     ui->setupUi (this);
     this->setWindowTitle ("Поиск по базе данных");
+
+    // подключили БД
+    db = QSqlDatabase::addDatabase ("QIBASE");
+    db.setUserName ("SYSDBA");
+    db.setPassword ("masterkey");
+    db.setDatabaseName ("G:\\ProjectQT\\QT_PhoneBook\\resources\\test25.fdb");
+    // контроль ошибок при подключении
+    if (db.open ()) {
+        ui->statusbar->showMessage ("The database connection is open.", 2000);
+        qDebug ("The database connection is open.");
+    }
+    else {
+        ui->statusbar->showMessage (db.lastError ().text (), 2000);
+        ui->statusbar->showMessage (db.lastError ().text (), 2000);
+
+        qDebug () << db.lastError ().text ();
+    };
 }
 
 QT_PhoneBook::~QT_PhoneBook ()
