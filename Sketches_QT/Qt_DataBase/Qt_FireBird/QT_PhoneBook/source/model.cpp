e87cfcfbbf856a0e571f02511dba34606aef274e
Den-Git-CPP <madim-neodart@rambler.ru>
2023-11-06 23:39:44 +0300

    correct string in model correct connect bd

--- Sketches_QT/Qt_DataBase/Qt_FireBird/QT_PhoneBook/source/model.cpp
+++ Sketches_QT/Qt_DataBase/Qt_FireBird/QT_PhoneBook/source/model.cpp
@@ -6,7 +6,7 @@ QVariant Model::data (const QModelIndex& idx, int role) const
 {
     if (idx.isValid () && (idx.column () == 0 || idx.column () == 2) && role == Qt::DisplayRole) {
         QString result = QSqlTableModel::data (idx, role).toString ();
-        result.replace ("%20", " ");
+        result.replace (QString ("%20"), QString (" "));
         return result;
     }
     return QSqlTableModel::data (idx, role);
--- Sketches_QT/Qt_DataBase/Qt_FireBird/QT_PhoneBook/source/qt_phonebook.cpp
+++ Sketches_QT/Qt_DataBase/Qt_FireBird/QT_PhoneBook/source/qt_phonebook.cpp
@@ -55,10 +55,41 @@ void QT_PhoneBook::on_lineEdit_TlfNumber_textChanged (const QString& arg1)
 void QT_PhoneBook::connect_db ()
 {
     // подключили БД
-    db = QSqlDatabase::addDatabase ("QIBASE");
-    db.setUserName ("SYSDBA");
-    db.setPassword ("masterkey");
-    db.setDatabaseName ("E:\\ProjectQT\\QT_PhoneBook\\resources\\test_rpr.fdb");
+    //db = QSqlDatabase::addDatabase ("QIBASE");
+    //db.setUserName ("SYSDBA");
+    //db.setPassword ("masterkey");
+    //db.setDatabaseName ("E:\\ProjectQT\\QT_PhoneBook\\resources\\test_rpr.fdb");
+    
+
+    // const QString hostName      = "";
+    const QString dbFileName = "C:\\test_rpr.fdb";
+    const QString userName   = "SYSDBA";
+    const QString password   = "masterkey";
+    const QString serverType = "1";
+    //   const QString clientLibrary = QString ("C:\\QTPROJECT\\build-untitled-MinGW_11_2_0_64-Debug\\fbclient.dll");
+    QSqlDatabase db = QSqlDatabase::addDatabase ("QIBASE", "Connection_1");
+    if (!db.isValid ()) {
+        qDebug () << db.lastError ().text ();
+    }
+    else {
+        qDebug () << "The database is valid.";
+    }
+    // db.setHostName (hostName);
+    db.setDatabaseName (dbFileName);
+    db.setUserName (userName);
+    db.setPassword (password);
+    db.setConnectOptions (QString ("Database=%1;"
+                                   "User=%2;"
+                                   "Password=%3;"
+                                   "ServerType=%4;"
+                                   "ClientLibrary=%5;")
+                            .arg (dbFileName)
+                            .arg (userName)
+                            .arg (password)
+                            .arg (serverType)
+      //  .arg (clientLibrary)
+    );
+
     // контроль ошибок при подключении
     if (db.open ()) {
         ui->statusbar->showMessage ("The database connection is open.", 2000);
@@ -78,6 +109,15 @@ void QT_PhoneBook::init_model ()
                      "JOIN rgroups ON rabonents.GROUPID= rgroups.id "
                      "ORDER BY rabonents.NAME",
       db);
+
+    //    QSqlQuery query ("SELECT rabonents.NAME, rphones.PHONE, rgroups.NAME "
+    //                     "FROM rabonents "
+    //                     "JOIN rphones ON rabonents.ID= rphones.abonid "
+    //                     "JOIN rgroups ON rabonents.GROUPID= rgroups.id "
+    //                     "ORDER BY rabonents.NAME",
+    //      db);
+    //    query.exec ();
+    //    model->setQuery (query);
 }
 
 void QT_PhoneBook::update_date_in_model (const QString& query_string)
