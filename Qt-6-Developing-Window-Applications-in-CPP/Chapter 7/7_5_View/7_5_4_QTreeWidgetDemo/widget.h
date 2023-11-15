cce72b03ef3bcd1eee40a68cf275627e0a79840d
Den-Git-CPP <madim-neodart@rambler.ru>
2023-11-15 16:19:42 +0300

    Add 7_5_4_QTreeWidgetDemo

---
+++ Qt-6-Developing-Window-Applications-in-CPP/Chapter 7/7_5_View/7_5_4_QTreeWidgetDemo/widget.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QWidget>
+#include <QTreeWidgetItem>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_treeWidget_itemClicked(QTreeWidgetItem *item, int column);
+
+private:
+    QTreeWidgetItem *addRootOrganization(QString company, QString purpose);
+    QTreeWidgetItem *addChildOrganization(QTreeWidgetItem *parent,QString branch,
+                                          QString description);
+    Ui::Widget *ui;
+};
+ 
