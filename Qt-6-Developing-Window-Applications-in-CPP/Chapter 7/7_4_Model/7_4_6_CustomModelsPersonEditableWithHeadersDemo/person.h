abf7a5960b35fe3980f4228b9e28181eba780da5
Den <madim-neodart@rambler.ru>
2023-11-16 14:44:48 +0300

    Add 7_4_6_CustomModelsPersonEditableWithHeadersDemo

---
+++ Qt-6-Developing-Window-Applications-in-CPP/Chapter 7/7_4_Model/7_4_6_CustomModelsPersonEditableWithHeadersDemo/person.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <QObject>
+
+class Person : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Person(QObject *parent = nullptr);
+    Person(const QString &names, const QString &favoritecolor,
+           const int &age , QObject * parent = nullptr);
+
+    QString names() const;
+    QString favoriteColor() const;
+    int age() const;
+    void setNames(QString names);
+    void setFavoriteColor(QString favoriteColor);
+    void setAge(int age);
+
+signals:
+    void namesChanged(QString names);
+    void favoriteColorChanged(QString favoriteColor);
+    void ageChanged(int age);
+
+private:
+    QString m_names;
+    QString m_favoriteColor;
+    int m_age;
+};
