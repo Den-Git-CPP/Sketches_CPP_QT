24f7c9fad34999563577ac394f4a3fb724c983b6
Den-Git-CPP <madim-neodart@rambler.ru>
2023-11-18 17:33:06 +0300

    Add 4_2_DragDropImageDemo

---
+++ Qt-6-Developing-Window-Applications-in-CPP/Chapter 4/4_10_DragAndDrop/4_2_DragDropImageDemo/widget.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+
+    // QWidget interface
+protected:
+    void dragEnterEvent(QDragEnterEvent *event) override;
+    void dragMoveEvent(QDragMoveEvent *event) override;
+    void dragLeaveEvent(QDragLeaveEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
+
+private:
+    bool isImage(QString fullpath);
+};
+
+
