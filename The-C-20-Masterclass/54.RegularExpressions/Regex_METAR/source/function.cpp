c861181924802b841712fffe60a1345e93f31f0d
Den-Git-CPP <madim-neodart@rambler.ru>
2024-01-24 23:39:59 +0300

    Correct work Load_AMOFSG_Dictionary

--- The-C-20-Masterclass/54.RegularExpressions/Regex_METAR/source/function.cpp
+++ The-C-20-Masterclass/54.RegularExpressions/Regex_METAR/source/function.cpp
@@ -152,14 +152,14 @@ void Function::Load_AMOFSG_Dictionary ()
         std::string item1{}, item2{};
         while (infile) {
             std::getline (infile, item1, '/'), getline (infile, item2);
-            // delete /r /n unix windows
-            item2.erase (std::remove (item2.begin (), item2.end (), '\r'), item2.end ());
-            item2.erase (std::remove (item2.begin (), item2.end (), '\n'), item2.end ());
-
             if (item1 != "") {
-                All_Dictionary [item1] = item2;
+                // delete /r /n unix windows]
+                if ((item2.back () == '\r') || (item2.back () == '\n')) {
+                    item2.pop_back ();
+                    All_Dictionary [item1] = item2;
+                }
             }
+            infile.close ();
         }
-        infile.close ();
     }
 }
