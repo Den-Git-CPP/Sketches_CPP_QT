9775d360e5226d9eb37cccf7099efb55655e4407
Den-Git-CPP <madim-neodart@rambler.ru>
2024-01-29 23:32:14 +0300

    Storage_Forecast::split over sstreame

--- The-C-20-Masterclass/54.RegularExpressions/Regex_METAR/include/storage_forecast.h
+++ The-C-20-Masterclass/54.RegularExpressions/Regex_METAR/include/storage_forecast.h
@@ -4,14 +4,9 @@
 #include <memory>
 #include <string>
 #include <regex>
-// split
-#include <ranges>
-#include <string_view>
 
 #include "./include/forecast.h"
 #include "./include/function.h"
-
-using std::operator""sv;
 
 class Storage_Forecast : public Function {
   public:
@@ -20,7 +15,7 @@ class Storage_Forecast : public Function {
     void split (const std::string& in_forecast_str);
     void convert_word_to_Forcast (std::unique_ptr<Forecast>& u_ptr_forcast, const std::string& in_forecast_str);
 
-    std::string RawTAF{ "TAF" };
-    std::string RawMETAR{ "MATER" };
+    std::string RawTAF{};
+    std::string RawMETAR{};
     std::vector<std::unique_ptr<Forecast>> all_Forecast{};
 };
