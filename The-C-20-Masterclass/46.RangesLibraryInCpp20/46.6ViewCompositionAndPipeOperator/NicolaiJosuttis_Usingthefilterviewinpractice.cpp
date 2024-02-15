63c0bb11532594e81da1b6b4d77777dadc422542
Den-Git-CPP <madim-neodart@rambler.ru>
2024-02-15 21:29:23 +0300

    Add NJosuttis  Processing Containers and Views

--- The-C-20-Masterclass/46.RangesLibraryInCpp20/46.6ViewCompositionAndPipeOperator/NicolaiJosuttis_Usingthefilterviewinpractice.cpp
+++ The-C-20-Masterclass/46.RangesLibraryInCpp20/46.6ViewCompositionAndPipeOperator/NicolaiJosuttis_Usingthefilterviewinpractice.cpp
@@ -3,7 +3,8 @@
 #include <algorithm>
 #include <vector>
 #include <set>
-
+#include <map>
+#include <list>
 void print (const auto& coll)
 {
     for (const auto& elem : coll) {
@@ -12,11 +13,19 @@ void print (const auto& coll)
     std::cout << "\n";
 }
 
+void print2 (auto&& coll)
+{
+    for (const auto& elem : coll) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+}
 int main ()
 {
 
     std::vector<int> coll1{ 0, 15, 47, 11, 42, 2 };
     std::set<int> coll2{ 0, 15, 47, 11, 42, 2 };
+
     print (coll1);                       // 0, 15, 47, 11, 42, 2
     print (coll2);                       // 0, 15, 47, 11, 42,
     print (std::views::take (coll1, 3)); // 0 15 47 // print first 3 elem
@@ -28,6 +37,71 @@ int main ()
            | std::views::transform ([] (auto v) { // прибавили в конце "s" // 0s 2s 11s
                  return std::to_string (v) + 's';
              }));
+
+    std::map<std::string, int> composers{
+        {       "Bach", 1965},
+        {     "Mozart", 1756},
+        {  "Beethoven", 1770},
+        {"Tchaikovsky", 1840},
+        {     "Chopin", 1810},
+        {    "Vivaldi", 1675}
+    };
+
+    namespace vws = std::views;
+    for (const auto& elem : composers
+                              | vws::filter ([] (const auto& e) // since 1700
+                                {
+                                    return e.second >= 1700;
+                                })
+                              | vws::take (3) // first 3
+                              | vws::keys     // only names
+    ) {
+        std::cout << "-" << elem << "\n"; //-Bach
+                                          //-Beethoven
+                                          // -Chopin
+    }
+
+    auto v = std::views::iota (1) // generate 1,2,3..
+             | std::views::filter ([] (auto val) {
+                   return val % 3 == 0;
+               })
+             | std::views::drop (3) // skip 3
+             | std::views::take (3) // take next 3
+             | std::views::transform ([] (auto val) {
+                   return std::to_string (val) + 's';
+               });
+    for (const auto& elem : v) {          //-12s
+        std::cout << "-" << elem << "\n"; //-15s
+    }                                     //-18s
+
+    // Processing Containers and Views
+    std::vector<int> vec{ 0, 15, 47, 11, 42, 2 };
+    std::list<int> lst{ 0, 15, 47, 11, 42, 2 };
+    print (vec);
+    print (lst);
+
+    print (vec | std::views::take (3));
+    print (lst | std::views::take (3));
+    print (vec | std::views::drop (3));
+    // print (lst | std::views::drop (3)); // Compile-time Error
+    print (std::ranges::subrange{ lst | std::views::drop (3) }); // OK
+
+    auto less20 = [] (auto val) {
+        return val < 20;
+    };
+    print2 (vec | std::views::filter (less20)); // OK
+    print2 (lst | std::views::filter (less20)); // OK
+
+    std::vector<int> vec_1{ 0, 15, 47, 11, 42, 2 };
+    print (vec_1); // 0 15 47 11 42 2
+    auto isEven = [] (auto&& i) {
+        return i % 2 == 0;
+    };
+    auto vec_1_Even = vec_1 | std::views::filter (isEven);
+    for (int& i : vec_1_Even) {
+        i += 2;
+    }
+    print (vec_1); // 2 15 47 11 44 4
 
     return 0;
 }
