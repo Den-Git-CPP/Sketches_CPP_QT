956e29614253cc4524cf49723a1beb332ae04b25
Den-Git-CPP <madim-neodart@rambler.ru>
2024-03-04 22:39:44 +0300

    Add std:optional Devide/cpp

---
+++ The-C-20-Masterclass/19.GettingThingsOutOfFuntions/19.13OptionalOutputWithStdOptional/devide.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <optional>
+
+std::optional<int> divide(int num1, int num2) {
+    if (num2 != 0) {
+        return num1/num2;
+    }
+    return std::nullopt; // Indicates no type-safe value
+}
+
+int main() {
+    auto result = divide(10, 2);                                // Чтобы сделать вывод в std::optional<int>, используемый для экономии времени
+    if (result.has_value()) {                                   // has_value проверяет, возвращается ли значение типа
+        std::cout << "Result: " << result.value() << std::endl; // value возвращает значение, поскольку аргументы функции верны
+    } else {
+        std::cout << "Division by zero" << std::endl;
+    }
+
+    return 0;
+}
